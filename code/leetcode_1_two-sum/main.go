9daedbc57d19051041129fe9cad383f2bbb09f52
shexuemeng <shexuemeng@cmcm.com>
2021-06-19 17:42:16 +0800

    [leetcode#1]

--- code/leetcode_1_two-sum/main.go
+++ code/leetcode_1_two-sum/main.go
@@ -15,28 +15,12 @@ func twoSum(nums []int, target int) []int {
 		indexMap[v] = append(indexMap[v], k)
 	}
 	sort.Ints(nums)
-	vaildNums := []int{}
-	for k := len(nums) - 1 ; k > 0; k-- {
-		if nums[k] + nums[0] < target {
-			vaildNums = nums[0:k+1]
-			break
-		} else if nums[k] + nums[0] == target {
-			t1 := indexMap[nums[k]][len(indexMap[nums[k]])-1]
-			if len(indexMap[nums[k]]) == 1 {
-				indexMap[nums[k]] = []int{}
-			} else {
-				indexMap[nums[k]] = indexMap[nums[k]][0:len(indexMap[nums[k]])-1]
-			}
-			t2 := indexMap[nums[0]][0]
-			return []int{t1, t2}	
+	for k := 0 ; k < len(nums) - 1; k++ {
+		if 2 * nums[k] == target && len(indexMap[nums[k]]) == 2 {
+			return indexMap[nums[k]]
 		}
-	}
-	for k := 0 ; k < len(vaildNums) - 1; k++ {
-		if 2 * vaildNums[k] == target && len(indexMap[vaildNums[k]]) == 2 {
-			return indexMap[vaildNums[k]]
-		}
-		if res, status := indexMap[target-vaildNums[k]]; status {
-			return []int{res[0], indexMap[vaildNums[k]][0]}
+		if res, status := indexMap[target-nums[k]]; status {
+			return []int{res[0], indexMap[nums[k]][0]}
 		}
 	}
 	return []int{}
