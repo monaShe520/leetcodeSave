96b98b734a873561167dae544bdd0e0eeef1debc
shexuemeng <shexuemeng@cmcm.com>
2021-06-24 21:10:55 +0800

    [leetcode#19]

--- code/leetcode_19_remove-nth-node-from-end-of-list/main.go
+++ code/leetcode_19_remove-nth-node-from-end-of-list/main.go
@@ -14,7 +14,6 @@ type ListNode struct {
 	Val int
 	Next *ListNode
 }
-
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	if head.Next == nil && n == 1 {
 		return nil
@@ -40,11 +39,46 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	return head
 }
 
+func removeNthFromEnd1(head *ListNode, n int) *ListNode {
+	if head.Next == nil && n == 1 {
+		return nil
+	}
+	first := 0
+	firstHead := head
+	lastHead := head
+	last := -n
+	for {
+		if last >= 0 {
+			lastHead = lastHead.Next
+		}
+		last = last + 1
+		first = first + 1
+		firstHead = firstHead.Next
+		if firstHead.Next == nil {
+			if last < 0 {
+				return head.Next
+			}
+			lastHead.Next = lastHead.Next.Next
+			break
+		}
+	}
+	return head
+
+}
 func main() {
 	l1 := &ListNode{
 		Val: 1,
 		Next: &ListNode{
 			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+					},
+				},
+			},
 		},
 	}
 	
